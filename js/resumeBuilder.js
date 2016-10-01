307b9184522ddfb8b25bc68162e1c69cdea82aa2
Muhammad Essam <mohammedessam699@gmail.com>
2016-10-01 10:19:15 +0800

    this is chaning name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formatedname = HTMLheaderName.replace("%data%", "Muhammad Essam");
+var formatedrole = HTMLheaderRole.replace("%data%", "Front-End Web Dev");
+$("#header").prepend(formatedrole);
+$("#header").prepend(formatedname);
+var skills = ["awesomeness","programming",{"name":"30"},"sdfgdsg"];
+$("#main").append(skills[2]);
